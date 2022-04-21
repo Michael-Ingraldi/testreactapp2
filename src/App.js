9dc227a97967bd2b988deb8135dbfb696c679f03
BuildTools <unconfigured@null.spigotmc.org>
2022-04-21 14:13:10 -0400

    Repo Creation?

--- src/App.js
+++ src/App.js
@@ -1,25 +1,80 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import Button from '@mui/material/Button';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+    const [weather, setWeather] = useState({});
+
+    const [ locations, setLocations] = useState("london");
+    
+    const [photos, setPhotos] = useState([]);
+    const API_KEY = "6b3a8fff86086a87f11c0397aca11ea4";
+    const API_KEYUNSPLASH = "sDlaR75srgLhSg5nCV5XkakktzF9-7cN_2E3jFgTfjA";
+    
+    useEffect(() => {
+      submit();
+    }, []);
+
+    function submit(){
+      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${locations}&appid=${API_KEY}&units=metric`)
+      .then((res) => {
+        if (res.ok){
+          console.log(res.status);
+          return res.json();
+        } else {
+          if (res.status === 404)
+          {
+            return alert("Oops there is an error! (wrong location)");
+          }
+          alert("Oops there seems to be an error!");
+          throw new Error("You have an error");
+        }
+      })
+      .then((object) => {
+          setWeather(object);
+          console.log(weather);
+      })
+      .catch((error) => console.log(error));
+
+      fetch(`https://api.unsplash.com/search/photos?query=${locations}&client_id=${API_KEYUNSPLASH}`)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();  
+        } else {
+          throw new Error("You made a mistake");
+        }
+      })
+      .then((data) => {
+        console.log(data);
+        setPhotos(data?.results[0]?.urls?.raw)
+      })
+      .catch((error) => console.log(error));
+    
+
+    /////////////////////
+
+  ///////////////////////
+
+    }
+
+    return (
+      <div className="App">
+      
+        <div className="wrapper">
+          <div className="Search">
+            <input type="text" value={locations} onChange={(e) => setLocations(e.target.value)} placeholder="Enter Location" className="location_input"/>
+            <Button onClick={submit}>Search</Button>
+          </div>
+
+          <div className="app_data">
+            <p className="temp">Current Temp: {weather?.main?.temp}</p>
+          </div>
+          <img className="app_image" src={photos} alt=""/>
+        </div>
+      </div>
+    );
+  }
 
 export default App;
---
+++ src/TempComp.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class TempComp extends React.Component
+{
+    render() {
+        return(<div className="Test">Linked to the App Component</div>)
+    }
+}
+
+export default TempComp;
