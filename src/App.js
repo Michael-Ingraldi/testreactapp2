58c87fe21ae3c72c5544d8b270943350a0e2ed48
BuildTools <unconfigured@null.spigotmc.org>
2022-04-22 00:40:36 -0400

    Update

--- src/App.js
+++ src/App.js
@@ -1,42 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 
 function App() {
 
-    const [weather, setWeather] = useState({});
-
-    const [ locations, setLocations] = useState("london");
+    const [locations, setLocations] = useState("apple");
     
     const [photos, setPhotos] = useState([]);
-    const API_KEY = "6b3a8fff86086a87f11c0397aca11ea4";
+
+    const [dictionary, setDictionary] = useState([{
+      shortdef:""
+    }]);
+
     const API_KEYUNSPLASH = "sDlaR75srgLhSg5nCV5XkakktzF9-7cN_2E3jFgTfjA";
+    const API_KEYDICTIONARY = "66b4f1dd-f951-4b72-a5b7-8824a9fd1e54";
     
     useEffect(() => {
       submit();
     }, []);
 
     function submit(){
-      fetch(`http://api.openweathermap.org/data/2.5/weather?q=${locations}&appid=${API_KEY}&units=metric`)
-      .then((res) => {
-        if (res.ok){
-          console.log(res.status);
-          return res.json();
-        } else {
-          if (res.status === 404)
-          {
-            return alert("Oops there is an error! (wrong location)");
-          }
-          alert("Oops there seems to be an error!");
-          throw new Error("You have an error");
-        }
-      })
-      .then((object) => {
-          setWeather(object);
-          console.log(weather);
-      })
-      .catch((error) => console.log(error));
 
       fetch(`https://api.unsplash.com/search/photos?query=${locations}&client_id=${API_KEYUNSPLASH}`)
       .then((res) => {
@@ -55,26 +38,47 @@ function App() {
 
     /////////////////////
 
-  ///////////////////////
+    fetch(`https://www.dictionaryapi.com/api/v3/references/collegiate/json/${locations}?key=${API_KEYDICTIONARY}`)
+    .then((res) => {
+      if (res.ok) {
+        return res.json();  
+      } else {
+        throw new Error("You made a mistake");
+      }
+    })
+    .then((object) => {
+      setDictionary(object);
+      console.log(dictionary);
+    })
+    .catch((error) => console.log(error));
+
+    ///////////////////////
 
     }
 
+
+    
     return (
+      
       <div className="App">
       
         <div className="wrapper">
+        <h1>Lil' Dictionary</h1>
           <div className="Search">
-            <input type="text" value={locations} onChange={(e) => setLocations(e.target.value)} placeholder="Enter Location" className="location_input"/>
-            <Button onClick={submit}>Search</Button>
+            <input type="text" value={locations} onChange={(e) => setLocations(e.target.value)} placeholder="Enter Word" className="location_input"/>
+            <br></br>
+            <Button variant="contained" onClick={submit}>Search</Button>
           </div>
-
+          
           <div className="app_data">
-            <p className="temp">Current Temp: {weather?.main?.temp}</p>
+
+            <p className="temp"><b>Definition:</b> {dictionary[0].shortdef}</p>
           </div>
           <img className="app_image" src={photos} alt=""/>
         </div>
       </div>
     );
-  }
+  } 
 
 export default App;
+
